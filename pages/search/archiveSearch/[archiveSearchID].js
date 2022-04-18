ef9bc43df4cde6f4387e71165da1561658896ea0
Davit Ter-Mkrtchyan <davitm944@gmail.com>
2022-04-18 13:36:16 +0400

    Minor bug fixes

--- pages/search/archiveSearch/[archiveSearchID].js
+++ pages/search/archiveSearch/[archiveSearchID].js
@@ -45,9 +45,11 @@ function Archive(props) {
 export default Archive;
 
 export async function getServerSideProps(context) {
+	console.log(context
+		.query.singleSearchID)
 	const res = await fetch(
 		`https://api.twitter.com/2/tweets/search/recent?max_results=30&tweet.fields=created_at&expansions=attachments.poll_ids,attachments.media_keys,author_id,geo.place_id,in_reply_to_user_id,referenced_tweets.id,entities.mentions.username,referenced_tweets.id.author_id&media.fields=duration_ms,height,media_key,non_public_metrics,organic_metrics,preview_image_url,promoted_metrics,public_metrics,type,url,width&place.fields=country,country_code,full_name,geo,id,name,place_type&user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld&query=${context
-			.query.singleSearchID}
+			.query.archiveSearchID}
     `,
 		{
 			headers : {
